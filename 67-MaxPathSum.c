ff07eb9fce5fe950d5e2f18bd29075e43b1f8b26
jackrvan <jack.vanier16@yahoo.ca>
2016-09-29 11:52:24 -0400

    Add files via upload

---
+++ 67-MaxPathSum.c
@@ -0,0 +1,56 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define NUMROWS 100
+
+/*Problem number 67 on project euler
+ * Start at top of array and add the larger number above it and set that to the new number
+ * make way to the bottom and then take the biggest number in the last row of the pyramid
+*/
+
+int *makeArray() {
+	FILE *fp;
+	int currentNum;
+	static int array[(NUMROWS*(NUMROWS+1))/2], a = 0;
+	fp = fopen("pyramid.txt", "r"); //Open pyramid.txt for reading.
+	while(fscanf(fp, "%d ", &currentNum) == 1) {
+		array[a] = currentNum;
+		++a;
+	}
+	fclose(fp);
+	return array;
+}
+
+int main() {
+	int numNumbers = (NUMROWS * (NUMROWS + 1))/2;
+	int *arrayNums = malloc(sizeof(int) * numNumbers);  //Variable for pointer to int with enough space for all of the integers in our file
+	arrayNums = makeArray();
+	int currentRowNumber, currentIndex = 1;
+	int firstNumberOfRow, knt;
+	for(currentRowNumber = 2, firstNumberOfRow = 1; currentRowNumber < (NUMROWS + 1); ++currentRowNumber) {
+		for(knt = 0; knt < currentRowNumber; ++knt) {
+			int temp, temp2;
+			if(firstNumberOfRow) {
+				arrayNums[currentIndex] = arrayNums[currentIndex] + arrayNums[currentIndex-(currentRowNumber - 1)]; //Add the current number we are on with the one above it and to the right
+				firstNumberOfRow = 0;
+				
+			} else if(knt == (currentRowNumber - 1)) {						// Last number in the row
+				arrayNums[currentIndex] = arrayNums[currentIndex] + arrayNums[currentIndex-(currentRowNumber)]; //Add the current number we are on with the one above it and to the left
+			} else {
+				temp = arrayNums[currentIndex] + arrayNums[currentIndex-(currentRowNumber - 1)]; //Add the current number we are on with the one above it adn to the right
+				temp2 = arrayNums[currentIndex] + arrayNums[currentIndex-(currentRowNumber)]; //Add the current number we are on with the one above it adn to the left
+				arrayNums[currentIndex] = (temp > temp2 ? temp : temp2); //Set it equal to the bigger number
+			}
+			++currentIndex;
+		}
+	}
+	int a = 0;
+	int max = 0;
+	while(arrayNums[a]) {
+		if(arrayNums[a] > max) {
+			max = arrayNums[a];
+		}
+		++a;
+	}
+	printf("%d\n", max);
+	return 0;
+}
